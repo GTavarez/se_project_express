d868a528342e15342bb570cb1d6dbf09be3464b8
Gtavarez <giselltavarez@gmail.com>
2025-09-08 17:05:22 -0400

    strusture set up and connecting to DB

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
