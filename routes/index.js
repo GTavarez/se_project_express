7e3c7e8aa08f1c0a18ff242e2225ff8ddf1e9999
Gtavarez <giselltavarez@gmail.com>
2025-10-13 16:26:14 -0400

    fixing errors from reviwere and reseting the login form

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const clothingItemRouter = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
 const { createUser, login } = require("../controllers/users");
 
-/* router.post("/signup", createUser);
-router.post("/signin", login); */
+router.post("/signup", createUser);
+router.post("/signin", login);
 
 router.use("/items", clothingItemRouter);
 router.use("/users", userRouter);
